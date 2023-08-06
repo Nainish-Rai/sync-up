857e7f0a16757e030b7f6de0590042c4fb62edb4
Nainish Rai <nainishraisjp@gmail.com>
2023-08-06 19:17:31 +0530

    intial commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+// socket io server
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+const PORT = process.env.PORT || 8000;
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
